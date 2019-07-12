5c86f33dd1b5f88daf06d2aaf68bb53e26f47b0a
letsky <925727227@qq.com>
2019-07-12 16:33:00 +0800

    添加注释

--- src/main/java/org/sharebook/service/CurdService.java
+++ src/main/java/org/sharebook/service/CurdService.java
@@ -7,9 +7,27 @@ package org.sharebook.service;
  */
 public interface CurdService<T, ID> {
 
+    /**
+     * 根据主键查找实体
+     * @param id 主键
+     */
     void findById(ID id);
-    void findAll();
+
+    /**
+     * 根据主键查找实体列表
+     * @param id 主键
+     */
     void findAllById(ID id);
+
+    /**
+     * 保存或更新实体
+     * @param t 实体
+     */
     void save(T t);
+
+    /**
+     * 删除实体
+     * @param id 主键
+     */
     void delete(ID id);
 }
