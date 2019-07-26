7d1aaf4de9144b57590b6aa8d31972b0b9f6c850
xiao <283710867@qq.com>
2019-07-26 17:13:51 +0800

    Revert "修改密码"

--- src/main/java/org/sharebook/servlet/ForgetServlet.java
+++ src/main/java/org/sharebook/servlet/ForgetServlet.java
@@ -1,12 +1,5 @@
 package org.sharebook.servlet;
 
-import org.apache.commons.codec.digest.Md5Crypt;
-import org.sharebook.model.User;
-import org.sharebook.service.UserService;
-import org.sharebook.service.impl.UserServiceImpl;
-import org.sharebook.utils.MD5Utils;
-import org.sharebook.utils.ResponseUtils;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,40 +9,6 @@ import java.io.IOException;
 
 @WebServlet(urlPatterns = "/forget")
 public class ForgetServlet extends HttpServlet {
-    private UserService userService=new UserServiceImpl();
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        String userName=req.getParameter("userName");
-        String password=req.getParameter("password");
-        String newPassword=req.getParameter("newPassword");
-        User user=userService.findUserByName(userName);
-        if (user!=null){
-            String salt = user.getSalt();
-            String encryptPassword= MD5Utils.md5(password,salt);
-            boolean res=user.getPassword().equals(encryptPassword);
-            if (res){
-                String saltt=MD5Utils.getSalt();
-                String ePassword=MD5Utils.md5(newPassword, saltt);
-                user.setPassword(ePassword);
-                user.setSalt(saltt);
-                boolean result=userService.modifyPassword(user);
-                if (result){
-                    ResponseUtils.write(resp,ResponseUtils.success());
-                }
-                else {
-                    ResponseUtils.write(resp,ResponseUtils.error());
-                }
-            }
-            else {
-                //密码有误
-
-            }
-        }
-        else {
-            //没有找到该已注册的用户
-        }
-    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
