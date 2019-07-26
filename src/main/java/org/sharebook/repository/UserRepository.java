740ae14813d4107c1d05f3d2ffa7a65d86bcbe28
letsky <925727227@qq.com>
2019-07-26 17:20:23 +0800

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit d3f85550c27c26d25e9772cf8c5d2aa86ce737d6, reversing
    changes made to e522ef202f323d53281ea262c0c5e2409ff12c0a.

--- src/main/java/org/sharebook/repository/UserRepository.java
+++ src/main/java/org/sharebook/repository/UserRepository.java
@@ -14,4 +14,6 @@ public interface UserRepository extends CurdRepository<User, Long> {
     List<User> findAll(int page, int size);
 
     long getUsersCount();
+
+    int modifyPassword(User user);
 }
